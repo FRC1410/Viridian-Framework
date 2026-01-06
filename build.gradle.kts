1156b9d093e7f4baeb9c31bdb88856d6bc70da81
Greek Question Mark <galaxyisfake@gmail.com>
2026-01-05 19:34:04 -0700

    Idiotproofing build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("org.frc1410.tooling.build.java") apply false
+// These lines should never read apply true ON THE FRAMEWORK, they should only be true if this is the robot project for the year(i.e. Rebuilt2026)    
+    id("org.frc1410.tooling.build.java") apply false    
     id("edu.wpi.first.GradleRIO") version "2025.3.2" apply false
 }
 

791817bf3e584c748d40173d3eb6bc47ee8b5684
Greek Question Mark <galaxyisfake@gmail.com>
2026-01-06 13:13:13 -0700

    Idiotproofing Main.java

--- robot/src/main/java/org/frc1410/viridian/Main.java
+++ robot/src/main/java/org/frc1410/viridian/Main.java
@@ -1,3 +1,6 @@
+//WAIT! this package below should read "org.frc1410.viridian" IF you are on Viridian Framework, if you've forked Viridian into the current year(like Rebuilt2026)
+//this should read org.frc1410.[namehere] AND your file path should read robot\src\main\java\org\frc1410\[name here]
+
 package org.frc1410.viridian;
 
 import edu.wpi.first.wpilibj.RobotBase;
